f99d55bb84d53245bcac2ee3848b48f4481dbd4b
Stepanr10 <jalbertoestepan@gmail.com>
2025-06-25 04:17:13 -0400

    Correcciones
    Se insertan registros nuevos a la base de datos.
    
    Se mejora la validación de la respuesta en RestCountryService.

--- src/main/java/org/ClientApi/infrastructure/external/RestCountryService.java
+++ src/main/java/org/ClientApi/infrastructure/external/RestCountryService.java
@@ -1,7 +1,6 @@
 package org.ClientApi.infrastructure.external;
 
 import jakarta.enterprise.context.ApplicationScoped;
-
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
@@ -18,15 +17,26 @@ public class RestCountryService {
 
     public String getNationality(String isoCode) {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://restcountries.com/v3.1/alpha/" + isoCode)).GET().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://restcountries.com/v3.1/alpha/" + isoCode))
+                    .GET()
+                    .build();
 
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Cuando la respuesta sea diferente a 200
+            if (response.statusCode() != 200) {
+                System.err.println("Error de API RestCountryService: status=" + response.statusCode() + ", body=" + response.body());
+                return "Error: status=" + response.statusCode();
+            }
 
             JsonReader reader = Json.createReader(new StringReader(response.body()));
             JsonArray array = reader.readArray();
             JsonObject country = array.getJsonObject(0);
 
             return country.getJsonObject("demonyms").getJsonObject("eng").getString("m");
+
         } catch (Exception e) {
             return "Desconocido";
         }
